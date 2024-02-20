222273ffe5d987003cd440d6d32514d971344aec
GiacomoMilitello <g.militello95@gmail.com>
2024-02-20 11:58:48 +0100

    main Specials

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,25 @@
 <script>
 import Top from "./components/header/Top.vue";
+import Specials from "./components/main/Specials.vue"
 
 export default {
   components: {
     Top,
+    Specials,
   },
 };
 </script>
 
 <template>
+
   <header>
     <Top/>
   </header>
+
+  <main>
+    <Specials/>
+  </main>
+  
 </template>
 
 <style lang="scss">
---
+++ src/components/main/Specials.vue
@@ -0,0 +1,74 @@
+<script>
+export default {
+  name: "Specials",
+  components: {
+},
+  data() {
+        return {
+            specials: [
+                {
+                    price : '10',
+                    name : 'combo piccolo',
+                    description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nostrum odio temporibus'
+                },
+                {
+                    price : '20',
+                    name : 'combo mezzo',
+                    description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nostrum odio temporibus'
+                },
+                {
+                    price : '30',
+                    name : 'combo grande',
+                    description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nostrum odio temporibus'
+                },
+            ],
+        };
+    }
+};
+</script>
+
+<template>
+    <div class="row">
+        <figure class="col-6 d-flex align-items-center justify-content-center">
+            <img src="../../img/h1-img-7n.png" alt="">
+        </figure>
+        <div class="col-6 d-flex justify-content-center align-items-center">
+            <div class="w-50">
+                <h3>SPECIALS*</h3>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nostrum odio temporibus.</p>
+
+                <ul class="mt-4">
+                    <li v-for="(special, index) in specials" :key="index" class="row">
+                        <span class="col-2 fw-bold">$ {{ special.price }}</span>
+                        <h5 class="col-10"> {{ special.name }}</h5>
+                        <span class="col-2"></span>
+                        <p class="col-10">{{ special.description }}</p>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    
+</template>
+
+<style lang="scss" scoped>
+@use "../../styles/partials/variables" as *;
+@use "../../styles/partials/mixins" as *;
+
+    figure{
+        background-image: url(src/img/h1-img-4.jpg);
+        @include bg-cover
+    }
+    .col-6{
+        height: 700px;
+        span{
+            color: $orange;
+            font-size: 140%;
+            line-height: 33.6px;
+        }
+        h5{
+            line-height: 33.6px;
+        }
+    }
+
+</style>
